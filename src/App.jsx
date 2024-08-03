3859a16a93d9779092ea5d95b449f275f2f368c1
MATHEUS RATO MATIAS <3037495@itp.local>
2024-08-03 12:02:14 -0300

    Definição de Rotas

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Routes from "./routes";
+const bootstrap = require('bootstrap') 
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
---
+++ src/pages/resetpass/resetpass.jsx
@@ -0,0 +1,5 @@
+function Resetpass(){
+
+}
+
+export default Resetpass
